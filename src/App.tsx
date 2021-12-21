c847920f2de18fce5d18008f31f1549118a3c051
Anastasiia Shapovalova <a.shapovalova@eleks.com>
2021-12-21 09:20:07 +0200

    fix 404 page, remove redundant react-query code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { Router } from './router';
 import './App.css';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
+    <Router />
   );
 }
 
