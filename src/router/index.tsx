7fab34165d4b51508c7e428c9d12aba7af8d471e
Anastasiia Shapovalova <a.shapovalova@eleks.com>
2021-12-21 17:37:05 +0200

    fix routing

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from '../pages/Layout';
 import { routes } from './routes';
 
 export const Router =  function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={routes.layout.component}>
+        <Route path='/' element={<Layout />}>
           {Object.keys(routes).map((routeKey: string) => {
             const route = routes[routeKey];
-            if (!route.localPath) {
-              return null;
-            }
             const routeProps: { [propName: string]: any } = {
               element: route.component
             };
             const path = route.localPath;
-            if (!!path) {
-              routeProps.path = path;
-            } else {
+            if (!path) {
               routeProps.index = true;
+            } else {
+              routeProps.path = path;
             }
               return (<Route key={routeKey} {...routeProps} />);
           })}
