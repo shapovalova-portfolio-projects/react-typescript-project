997b6664c733d4166b1a984893fce69748948647
Anastasiia <nastikue@gmail.com>
2021-12-21 17:40:41 +0200

    Bugfix router (#3)
    * fix 404 page, remove redundant react-query code
    
    * fix routing
    
    Co-authored-by: Anastasiia Shapovalova <a.shapovalova@eleks.com>

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,9 @@
-import { Layout } from '../pages/Layout';
 import { Home } from '../pages/Home';
 import { MemoryLibrary } from '../pages/MemoryLibrary';
 import { PageNotFound } from '../pages/PageNotFound';
 import { ProjectRoutes } from '../types';
 
 export const routes: ProjectRoutes = {
-    layout: {
-        component: <Layout />,
-        isPublic: true,
-    },
     home: {
         path: '/',
         component: <Home />,
